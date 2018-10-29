fc704dfd458745bcf1994623ace42e4795be9698
Michael Alves <michael.paco.alves@gmail.com>
2018-10-29 04:02:42 -0300

    mongodb_uri fixed

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -13,7 +13,7 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production";
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const MONGODB_URI = prod ? `${process.env["MONGODB_URI"]}/todo-api-${ENVIRONMENT}` : `${process.env["MONGODB_URI_LOCAL"]}/todo-api-${ENVIRONMENT}`;
+export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : `${process.env["MONGODB_URI_LOCAL"]}/todo-api-${ENVIRONMENT}`;
 
 if (!SESSION_SECRET) {
   logger.error("No client secret. Set SESSION_SECRET environment variable.");
